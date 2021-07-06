fba0b52e17d3aa9012dd5570511430192db643d1
Akshay <akkii922@gmail.com>
2021-07-07 00:13:24 +0530

    added

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ const io = (module.exports.io = require('socket.io')(server, {
 const socketManager = require("./app/socketManager");
 io.on("connection", socketManager);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
