004a8632ea608af0d3a336edfe89870d8f372bc7
Akshay <akkii922@gmail.com>
2021-07-07 00:38:46 +0530

    .

--- server.js
+++ server.js
@@ -26,14 +26,12 @@ const io = (module.exports.io = require('socket.io')(server, {
 const socketManager = require("./app/socketManager");
 io.on("connection", socketManager);
 
-console.log("asdasdasd", path.join(__dirname, 'client/build'));
-
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
